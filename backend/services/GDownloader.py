25a626bd9dd4272405cd32fbe817d3ca66d7767b
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2025-11-07 13:35:36 +0530

    Refactor code to improve readability and add tiles creation method in pipeline to consume tiler service

--- backend/services/GDownloader.py
+++ backend/services/GDownloader.py
@@ -1,21 +1,27 @@
 import os
 from os.path import dirname as up
 from utils.utils import rand_str
+from utils.logger import logger
 import gdown
 
 class GDownloader:
-    def __init__(self):
-        pass
 
-    def download(self, file_id:str, file_name:str = f"aoi_{rand_str(5)}", format: str = ".tif", destination:str= "aoi"):
-        dest_folder = os.path.join(up(up(__file__)), "data", destination)
-        if not os.path.exists(dest_folder):
-            os.makedirs(dest_folder)
-        dest_path = os.path.join(dest_folder, f"{file_name}{format}")
+    def download(
+        self, 
+        file_id:str, 
+        file_name:str = f"aoi_{rand_str(5)}", 
+        format: str = ".tif", 
+        output_dir:str= "",
+    ) -> str:
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir, exist_ok=True)
+        dest_path = os.path.join(output_dir, f"{file_name}{format}")
+        
         try:
-            file_url = f"https://drive.google.com/uc?id={file_id}"
-            gdown.download(file_url, format=format, output=dest_path, quiet=False)
-            print(f"File downloaded successfully to {dest_path}")
+            gdrive_url = f"https://drive.google.com/uc?id={file_id}"
+            gdown.download(gdrive_url, format=format, output=dest_path, quiet=False)
+            logger.info(f"File downloaded successfully to {dest_path}")
+            return dest_path
         except Exception as e:
-            print(f"An error occurred while downloading the file: {e}")
-        return dest_path
+            logger.error(f"Failed to download file: {e}")
+            raise e
--- backend/services/InferencePipeline.py
+++ backend/services/InferencePipeline.py
@@ -1,29 +1,57 @@
 import os
 from typing import Optional
 from services.GDownloader import GDownloader
+from services.Tiler import Tiler
 from os.path import dirname as up
+
+from utils.logger import logger
 
 class InferencePipeline:
     def __init__(
         self, 
-        aoi_name: str = "aoi",
+        aoi_name: str = "",
         aoi_file_id:str = "", 
-        model_folder:str = "saved_model", 
-        tiles_folder:str = "tiles",
+        model_dir:str = "saved_model", 
+        tiles_dir:str = "tiles",
         gd:bool = True
         ):
-        self.model_filepath = os.path.join(up(up(__file__)), model_folder)
-        self.tiles_folder = os.path.join(up(up(__file__)), "data", tiles_folder)
+        self.model_dir = os.path.join(up(up(__file__)), model_dir)
+        self.tiles_dir = os.path.join(up(up(__file__)), "data", tiles_dir)
+        self.aoi_dir = os.path.join(up(up(__file__)), "data", "aoi")
         self.aoi_file_id = aoi_file_id
+        self.aoi_name = aoi_name
+        self.gd = gd
+        logger.info(f"Inference Pipeline initialized with model_dir: {self.model_dir}, tiles_dir: {self.tiles_dir}, aoi_name: {self.aoi_name}, gd: {self.gd}")
 
-        if gd and self.aoi_file_id == "":
+    def download_aoi(self):
+        if self.gd and self.aoi_file_id == "":
             raise ValueError("AOI file ID must be provided when gd is True.")
         else:
+            logger.info("Downloading AOI file...")
             # downloading actuall AOI file from gdrive
-            gd_downloader = GDownloader()
-            self.aoi_file_id = gd_downloader.download(file_id=self.aoi_file_id)
-        if not os.path.exists(self.tiles_folder):
-            os.makedirs(self.tiles_folder)
+            try:
+                gd_downloader = GDownloader()
+                self.aoi_file_path = gd_downloader.download(
+                    file_id=self.aoi_file_id,
+                    file_name=f"aoi_{self.aoi_name}",
+                    format=".tif",
+                    output_dir=self.aoi_dir
+                )
+                logger.info(f"AOI file downloaded to {self.aoi_file_path}")
+            except Exception as e:
+                logger.error(f"Error downloading AOI file: {e}")
     
     def tiles_creation_processing(self):
-        pass
+        if not os.path.exists(self.tiles_dir):
+            os.makedirs(self.tiles_dir, exist_ok=True)
+        logger.info("Creating tiles from AOI file...")
+        _output_dir = os.path.join(self.tiles_dir, self.aoi_name)
+        try:
+            tiler = Tiler()
+            tiler.create_tiles(
+                aoi_path=self.aoi_file_path,
+                output_dir=_output_dir
+            )
+            logger.info(f"Tiles created successfully at {_output_dir}")
+        except Exception as e:
+            logger.error(f"Error during tiling process: {e}")
